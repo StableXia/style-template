59fe085bb4b2cd785de804ce2a0d1cb117be10a0
夏稳 <xiawen@xiawendeMacBook-Pro.local>
2020-01-02 11:48:47 +0800

    docs: 添加基础样式

---
+++ build/less-build.config.js
@@ -0,0 +1,14 @@
+const { parallel, dest, src } = require('gulp')
+const less = require('gulp-less')
+
+function copyFile() {
+    return src('../styles/less/**').pipe(dest('../lib/less'))
+}
+
+function buildLess() {
+    return src('../styles/less/index.less')
+        .pipe(less())
+        .pipe(dest('../lib/less/index.css'))
+}
+
+exports.default = parallel(copyFile, buildLess)
